99b10e62a7f45c93c203c8351d57fc3f2d4fa450
wzx <834529118@qq.com>
2021-11-17 09:38:00 +0800

    修复文件缺失

---
+++ src/directive/modules/index.js
@@ -0,0 +1,8 @@
+/*
+ * @Description: 请输入当前文件描述
+ * @Author: @Xin (834529118@qq.com)
+ * @Date: 2021-11-17 09:34:22
+ * @LastEditTime: 2021-11-17 09:37:44
+ * @LastEditors: @Xin (834529118@qq.com)
+ */
+export default {}
